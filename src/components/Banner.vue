751567648db69d29229803bfe2912eca6fda2b7c
zihan <2411663991@qq.com>
2024-08-16 18:03:01 +1200

    Create & Update Banner Component

---
+++ src/components/Banner.vue
@@ -0,0 +1,100 @@
+<template>
+
+  <div class="container">
+
+    <div class="section">
+
+      <div class="text-section">
+        <div>
+          <!-- <el-text>Helping Students Acheive Their Future</el-text> -->
+          <el-text>{{ title }}</el-text>
+        </div>
+
+        <div>
+          <!-- <el-text>The Industry Internship System helps students through providing working experience and expertise.</el-text> -->
+          <el-text line-clamp="3">{{ content }}</el-text>
+        </div>
+
+        <div>
+          <el-button type="primary" @click="handleLinkButton(path)">Get Involved</el-button>
+        </div>
+
+      </div>
+
+      <div class="image-section">
+        <img :src="imgPath" />
+      </div>
+    </div>
+
+  </div>
+
+</template>
+
+<script lang="ts" setup>
+
+defineProps<{
+  title: string,
+  content: string,
+  path: string,
+  imgPath: string
+}>()
+
+const handleLinkButton = (path: string) => {
+  alert(path)
+}
+
+</script>
+
+<style scoped>
+.container {
+  width: 100%;
+}
+
+.section {
+  display: flex;
+  justify-content: space-between;
+  padding: 0 180px;
+}
+
+.text-section {
+  max-width: 60%;
+  display: block;
+}
+
+/* .image-section {
+  padding-top: 30px;
+} */
+
+.image-section>img {
+  width: 690px;
+  height: 400px;
+}
+
+.text-section > div:first-child, .text-section > div:nth-child(2), .text-section > div:last-child {
+  max-width: 485px;
+}
+
+.text-section > div:first-child > .el-text {
+  color: #FE7235;
+  font-weight: bold;
+  font-size: 45px;
+}
+
+
+.text-section > div:nth-child(2) > .el-text {
+  color: #777777;
+  font-size: 30px;
+}
+
+.text-section > div:last-child {
+  margin-top: 20px;
+}
+
+.el-button {
+  width: 100%;
+  height: 40px;
+}
+
+
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,8 @@
   <div class="container">
     <Header />
 
+    <Banner style="margin-top: 6rem;" :title="title" :content="content" :imgPath="backgroundImage" path="test" />
+
     <Footer style="margin-top: 5rem;" />
   </div>
 </template>
@@ -9,5 +11,13 @@
 <script lang="ts" setup>
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
+import Banner from '@/components/Banner.vue'
+import backgroundImage from '@/assets/Background Image.svg'
+import { ref } from 'vue'
+
+
+const title = ref('Helping Students Acheive Their Future')
+const content = ref('The Industry Internship System helps students through providing working experience and expertise.')
+
 
 </script>
